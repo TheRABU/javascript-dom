d11ff4aebe02bc8257859acb0bbae89afdd74633
TheRABU <mdfazlerabbi114@gmail.com>
2023-09-09 15:27:34 +0600

    Selecting Elements in the DOM

---
+++ app.js
@@ -0,0 +1,29 @@
+//DOM Manipulation 
+
+//GetElementById()
+const title = document.getElementById('main-heading');
+
+console.log(title)
+
+
+
+//GetElementByClassName()
+
+const listItem = document.getElementsByClassName('list-items');
+console.log(listItem)
+
+
+//GetElementsbyTagName()
+const listPara = document.getElementsByTagName('p');
+console.log(listPara)
+
+
+//querySelector()
+const container = document.querySelector('div');
+console.log(container) 
+
+
+//querySelectorAll()
+
+const containerAll = document.querySelectorAll('div');
+console.log(container) //this one selects all the divs 
